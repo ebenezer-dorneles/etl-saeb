c2d09f48323def1f2ab4bbb1d592494cc295ed40
Ebenézer Dorneles <ebenezerdorneles@gmail.com>
2026-07-27 18:18:11 -0300

    feat(recode-professores): fix items vectors for violence and climate to match INEP dictionary

--- R/feature_engineering/recode_professores.R
+++ R/feature_engineering/recode_professores.R
@@ -4,12 +4,13 @@ process_professores <- function(professores_raw) {
   itens_violencia <- c(
     "TX_Q135", "TX_Q136", "TX_Q137", "TX_Q138",
     "TX_Q139", "TX_Q140", "TX_Q141", "TX_Q142",
-    "TX_Q143", "TX_Q144", "TX_Q145", "TX_Q146"
+    "TX_Q143", "TX_Q144", "TX_Q145", "TX_Q146",
+    "TX_Q147"
   )
 
   itens_clima <- c(
-    "TX_Q120", "TX_Q121", "TX_Q122",
-    "TX_Q123", "TX_Q124", "TX_Q125", "TX_Q126"
+    "TX_Q120", "TX_Q122", "TX_Q123",
+    "TX_Q127", "TX_Q128", "TX_Q129", "TX_Q130"
   )
 
   professores_df <- professores_raw |>
